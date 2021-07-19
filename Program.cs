59ec158bc1b3bdc1ed95ba4c86df5228a8b80efa
manish-1498 <manishpradhan14121998@gmail.com>
2021-07-19 17:04:50 +0530

    [manish]:added:addition and subtraction of two numbers

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace sum_and_sumbtract_of_two_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter first number");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int sum = a + b;
+            int minus = a - b;
+            Console.WriteLine("Sum of two numbers is :" + sum);
+            Console.WriteLine("subtraction of two numbers is :" + minus);
+        }
+    }
+}
